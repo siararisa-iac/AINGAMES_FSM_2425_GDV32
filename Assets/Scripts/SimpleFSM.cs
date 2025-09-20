cbe81e01b3009e88ac4a148c9730ced955bfb0f9
siararisa-iac <arisa.alcantara@iacademy.edu.ph>
2025-09-20 15:50:02 +0800

    Revert "Merge branch 'main' of https://github.com/siararisa-iac/AINGAMES_FSM_2425_GDV32"
    This reverts commit 48c65c85c2ccec2eba0f2051c1d3326b4bf761c1, reversing
    changes made to ee4f9f7a7194201b7586bb86dc3744b36888c930.

--- Assets/Scripts/SimpleFSM.cs
+++ Assets/Scripts/SimpleFSM.cs
@@ -5,7 +5,7 @@ public class SimpleFSM : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("I hate Green!");
+        
     }
 
     // Update is called once per frame
